296e3ab387eb9a0e15030379fbcc31fbe6ad7d76
David Koncsol <davidkoncsol@Davids-MBP.lan1>
2019-11-05 19:40:52 -0800

    added circle skills to homepage

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
 
-// var circles = document.getElementById('index-content');
-// var initials = 'lol';
+var circles = document.getElementById('index-content');
+var skills = ['AWS','jQuery', 'Python','PHP','Restful APIs','MongoDB','Node.js','Express','Java','JavaScript','HTML','Django','CSS','PostgreSQL','React'];
 
-//   circles.addEventListener('click', function(evt) {
-//     // replace current line of code with this code
-//    addCircle( {
-//       initials: initials,
-//       x: evt.clientX,
-//       y: evt.clientY,
-//       dia: randomBetween(10,100),
-//       rgba: getRandomRGBA()
-//     });
-//   });
-
-
-
-// function clearCircle(){
-//   circles.innerHTML = '';
-// }
+  circles.addEventListener('click', function(evt) {
+    // replace current line of code with this code
+   addCircle( {
+      skills: skills,
+      x: evt.clientX,
+      y: evt.clientY,
+      dia: randomBetween(10,100),
+      rgba: getRandomRGBA()
+    });
+  });
 
 
 
+function clearCircle(){
+  circles.innerHTML = '';
+}
 
 
-// function addCircle({x, y, dia, rgba, initials}) {
-//   var el = document.createElement('div');
-//   el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
-//   el.style.top = y - Math.floor(dia / 2 + 0.5) + 480 + 'px';
-//   el.style.width = el.style.height = dia + 'px';
-//   el.style.backgroundColor = rgba;
-//   el.style.fontSize = Math.floor(dia / 3) + 'px';
-//   el.style.color = 'white';
-//   el.style.textAlign = 'center';
-//   el.style.lineHeight = dia + 'px';
-//   el.innerHTML = initials;
-//   circles.appendChild(el);
-// }
 
-// function randomBetween(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
 
-// function getRandomRGBA() {
-//   return ['rgba(', randomBetween(0, 255), ',', randomBetween(0, 255), ',',
-//     randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'].join('');
-// }
+
+function addCircle({x, y, dia, rgba, skills}) {
+  var el = document.createElement('div');
+  el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
+  el.style.top = y - Math.floor(dia / 2 + 0.5)  + 'px';
+  el.style.width = el.style.height = dia + 'px';
+  el.style.backgroundColor = rgba;
+  el.style.fontSize = Math.floor(dia / 3) + 'px';
+  el.style.color = 'white';
+  el.style.textAlign = 'center';
+  el.style.lineHeight = dia + 'px';
+  el.innerHTML = skills[randomBetween(0,15)];
+  circles.appendChild(el);
+}
+
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomRGBA() {
+  return ['rgba(', randomBetween(0, 255), ',', randomBetween(0, 255), ',',
+    randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'].join('');
+}
