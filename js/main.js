7152ac9388e6adfbe22df66589ce1aee4eb5344d
David Koncsol <davidkoncsol@davids-mbp.home>
2019-11-06 12:55:45 -0800

    updated styling and circles function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,59 @@
-
 var circles = document.getElementById('index-content');
-var skills = ['AWS','jQuery', 'Python','PHP','APIs','MongoDB','Node.js','Express','Java','JavaScript','HTML','Django','CSS','PostgreSQL','React'];
+var left = document.getElementById('left');
+var right = document.getElementById('right');
 
-  circles.addEventListener('click', function(evt) {
-    // replace current line of code with this code
-   addCircle( {
-      skills: skills,
-      x: evt.clientX,
-      y: evt.clientY,
-      dia: randomBetween(40,120),
-      rgba: getRandomRGBA()
-    });
+  
+// var button = document.getElementById('slide');
+// button.onclick = function () {
+//     var container = document.getElementById('container');
+//     scrollAmount = 0;
+//     var slideTimer = setInterval(function(){
+//         container.scrollLeft += 10;
+//         scrollAmount += 10;
+//         if(scrollAmount >= 100){
+//             window.clearInterval(slideTimer);
+//         }
+//     }, 25);
+// };
+
+// var back = document.getElementById('slideBack');
+// back.onclick = function () {
+//     var container = document.getElementById('container');
+//     scrollAmount = 0;
+//     var slideTimer = setInterval(function(){
+//         container.scrollLeft -= 10;
+//         scrollAmount += 10;
+//         if(scrollAmount >= 100){
+//             window.clearInterval(slideTimer);
+//         }
+//     }, 25);
+// };
+
+
+var skills = ['AWS', 'jQuery', 'Python', 'PHP', 'APIs', 'MongoDB', 'Node.js', 'Express', 'Java', 'JavaScript', 'HTML', 'Django', 'CSS', 'PostgreSQL', 'React', 'Socket.io'];
+
+right.addEventListener('click', function(){
+  document.getElementById('sidescroller').scrollLeft += 600;
+});
+left.addEventListener('click', function(){
+  document.getElementById('sidescroller').scrollLeft -= 600;
+});
+
+
+circles.addEventListener('click', function (evt) {
+  // replace current line of code with this code
+  addCircle({
+    skills: skills,
+    x: evt.clientX,
+    y: evt.clientY,
+    dia: randomBetween(40, 120),
+    rgba: getRandomRGBA()
   });
+});
 
 
 
-function clearCircle(){
+function clearCircle() {
   circles.innerHTML = '';
 }
 
@@ -23,17 +61,23 @@ function clearCircle(){
 
 
 
-function addCircle({x, y, dia, rgba, skills}) {
+function addCircle({
+  x,
+  y,
+  dia,
+  rgba,
+  skills
+}) {
   var el = document.createElement('span');
   el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
-  el.style.top = y - Math.floor(dia / 2 + 0.5)  + 'px';
+  el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
   el.style.width = el.style.height = dia + 'px';
   el.style.backgroundColor = rgba;
   el.style.fontSize = Math.floor(dia / 5) + 'px';
   el.style.color = 'white';
   el.style.textAlign = 'center';
   el.style.lineHeight = dia + 'px';
-  el.innerHTML = skills[randomBetween(0,14)];
+  el.innerHTML = skills[randomBetween(0, skills.length - 1)];
   circles.appendChild(el);
 }
 
@@ -43,5 +87,6 @@ function randomBetween(min, max) {
 
 function getRandomRGBA() {
   return ['rgba(', randomBetween(0, 255), ',', randomBetween(0, 255), ',',
-    randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'].join('');
+    randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'
+  ].join('');
 }
